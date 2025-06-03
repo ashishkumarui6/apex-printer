e3abee1b4ab115df5fddafa95f4c9bab9fc9e0e2
ashishkumarui6 <ashishkumarui6@gmail.com>
2025-06-03 12:45:16 +0530

    add card icon

--- src/components/DealsCard/Card/index.jsx
+++ src/components/DealsCard/Card/index.jsx
@@ -3,10 +3,8 @@ import styles from "./index.module.css";
 const Card = ({ img, name, title }) => {
   return (
     <div className={styles.card}>
-      <div className={styles.card_img}>
-        <img src={img} alt={title} />
-      </div>
-      <div className={styles.name}>{name}</div>
+      <img src={img} alt={title} />
+      <h5 className={styles.name}>{name}</h5>
     </div>
   );
 };
--- src/components/DealsCard/index.jsx
+++ src/components/DealsCard/index.jsx
@@ -1,11 +1,49 @@
 import React from "react";
 import styles from "./index.module.css";
 import Card from "./card";
+import graphic_img from "../../assets/graphic_img.png";
+import { Link } from "react-router";
+
+// Define card data
+const cards = [
+  { img: graphic_img, title: "Deal 1", name: "Flex Board", link: "/flexBoard" },
+  {
+    img: graphic_img,
+    title: "Deal 2",
+    name: "School Stationary",
+    link: "/schoolstationary",
+  },
+  {
+    img: graphic_img,
+    title: "Deal 3",
+    name: "Graphic Designing",
+    link: "/graphicdesigning",
+  },
+  {
+    img: graphic_img,
+    title: "Deal 4",
+    name: "Invitation Card",
+    link: "/invitationcard",
+  },
+  {
+    img: graphic_img,
+    title: "Deal 5",
+    name: "Prospectus",
+    link: "/prospectus",
+  },
+];
+
 const DealsCard = () => {
   return (
     <div className={styles.dealsCard}>
-      <ul className={styles.content}>
-        <Card />
+      <ul className={styles.cardGrid}>
+        {cards.map((it, ind) => (
+          <li key={ind} className={styles.cardItem}>
+            <Link key={ind} to={it.link}>
+              <Card img={it.img} title={it.title} name={it.name} />
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
